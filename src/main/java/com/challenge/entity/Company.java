0222565043afea8353e4750822f89f9c070cf085
Adriano Avelino <adriano-st777@hotmail.com>
2020-07-11 11:52:22 -0300

    adiciona anotações da entity Company

--- src/main/java/com/challenge/entity/Company.java
+++ src/main/java/com/challenge/entity/Company.java
@@ -6,13 +6,20 @@ import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "company")
 public class Company implements Serializable {
 
@@ -23,12 +30,17 @@ public class Company implements Serializable {
     private Integer id;
     
     @Column
+    @NotNull
+    @Size(max = 100)
     private String name;
     
     @Column
+    @NotNull
+    @Size(max = 50)
     private String slug;
     
-    @Column
+    @Column(updatable = false)
+    @CreatedDate
     private Timestamp createdAt;
     
     @OneToMany(mappedBy = "company")
