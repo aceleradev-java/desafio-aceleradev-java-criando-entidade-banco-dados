1d313e1685c3d06c21eab56ebb05480b03631ecb
Adriano Avelino <adriano-st777@hotmail.com>
2020-07-11 00:39:00 -0300

    adiciona anotações da entity Challenge

--- src/main/java/com/challenge/entity/Challenge.java
+++ src/main/java/com/challenge/entity/Challenge.java
@@ -7,13 +7,20 @@ import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "challenge")
 public class Challenge implements Serializable {
 
@@ -24,17 +31,23 @@ public class Challenge implements Serializable {
     private Integer id;
     
     @Column(length=100)
+    @NotNull
+    @Size(max = 100)
     private String name;
     
     @Column(length=50)
+    @NotNull
+    @Size(max = 50)
     private String slug;
     
-    @Column
+    @Column(updatable = false)
+    @CreatedDate
     private Timestamp createdAt;
     
     @OneToMany(mappedBy = "challenge")
     private Set<Submission> submissions;
     
+
     @OneToMany(mappedBy = "challenge", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Acceleration> accelerations;
 
