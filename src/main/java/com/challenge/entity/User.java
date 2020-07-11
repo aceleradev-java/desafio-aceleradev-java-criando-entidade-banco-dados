f641f04adec70fca5216eaa6c37341e7ce13d052
Adriano Avelino <adriano-st777@hotmail.com>
2020-07-11 00:30:59 -0300

    atualiza anotações da entity User

--- src/main/java/com/challenge/entity/User.java
+++ src/main/java/com/challenge/entity/User.java
@@ -7,35 +7,53 @@ import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "user")
 public class User implements Serializable {
     
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
     
     @Column(length = 100)
+    @NotNull
+    @Size(max = 100)
     private String fullName;
     
     @Column(length = 100)
+    @NotNull
+    @Size(max = 100)
+    @Email
     private String email;
     
     @Column(length = 50)
+    @NotNull
+    @Size(max = 50)
     private String nickname;
     
     @Column(length = 255)
+    @NotNull
+    @Size(max = 255)
     private String password;
     
-    @Column
+    @Column(updatable = false)
+    @CreatedDate
     private Timestamp createdAt;
     
     @OneToMany(mappedBy = "user")
